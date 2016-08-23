bd3c570867a908c083ccabb0a9daae4d4ddc02d5
Allen Chen <allen.chen@autodesk.com>
2016-08-23 14:37:41 +0800

    - SNS Action with arg2 - Storage threshold change to 10GB for reasonable

--- put-aws-rds-cw-alarm.sh
+++ put-aws-rds-cw-alarm.sh
@@ -8,22 +8,31 @@
 # ALARMs
 #       CPUUtilization >= 80%
 #       DB Connections >= 100
-#       FreeStorageSpace =< 1GB
+#       FreeStorageSpace =< 10GB
 #       FreeableMemory =< 1GB
 #
 #===============================================================================
 
 RDS_IDENTIFIER=$1
+SNS_TOPIC_ARN=$2
 
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 RDS_NAME "
+    exit 0
+fi
 if [ -z ${RDS_IDENTIFIER} ]; then
-    echo "Usage: $0 RDS_NAME"
+    echo "Usage: $0 RDS_NAME "
+    exit 0
+fi
+if [ -z ${SNS_TOPIC_ARN} ]; then
+    echo "Usage: $0 RDS_NAME "
     exit 0
 fi
 
-aws cloudwatch put-metric-alarm --alarm-name awsrds-${RDS_IDENTIFIER}-High-CPU-Utilization --alarm-description "CPU Usage >=80% for 15 minutes" --metric-name "CPUUtilization" --namespace "AWS/RDS" --statistic Average --period 300 --threshold 80 --comparison-operator GreaterThanOrEqualToThreshold --dimensions Name=DBInstanceIdentifier,Value=${RDS_IDENTIFIER} --evaluation-periods 3 --unit Percent --alarm-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm" --ok-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm" --insufficient-data-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm"
+aws cloudwatch put-metric-alarm --alarm-name awsrds-${RDS_IDENTIFIER}-High-CPU-Utilization --alarm-description "CPU Usage >=80% for 15 minutes" --metric-name "CPUUtilization" --namespace "AWS/RDS" --statistic Average --period 300 --threshold 80 --comparison-operator GreaterThanOrEqualToThreshold --dimensions Name=DBInstanceIdentifier,Value=${RDS_IDENTIFIER} --evaluation-periods 3 --unit Percent --alarm-actions "${SNS_TOPIC_ARN}" --ok-actions "${SNS_TOPIC_ARN}" --insufficient-data-actions "${SNS_TOPIC_ARN}"
 
-aws cloudwatch put-metric-alarm --alarm-name awsrds-${RDS_IDENTIFIER}-High-DB-Connections --alarm-description "High DB Connections > 100" --metric-name "DatabaseConnections" --namespace "AWS/RDS" --statistic Average --period 300 --threshold 100 --comparison-operator GreaterThanOrEqualToThreshold --dimensions Name=DBInstanceIdentifier,Value=${RDS_IDENTIFIER} --evaluation-periods 1 --alarm-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm" --ok-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm" --insufficient-data-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm"
+aws cloudwatch put-metric-alarm --alarm-name awsrds-${RDS_IDENTIFIER}-High-DB-Connections --alarm-description "High DB Connections > 100" --metric-name "DatabaseConnections" --namespace "AWS/RDS" --statistic Average --period 300 --threshold 100 --comparison-operator GreaterThanOrEqualToThreshold --dimensions Name=DBInstanceIdentifier,Value=${RDS_IDENTIFIER} --evaluation-periods 1 --alarm-actions "${SNS_TOPIC_ARN}" --ok-actions "${SNS_TOPIC_ARN}" --insufficient-data-actions "${SNS_TOPIC_ARN}"
 
-aws cloudwatch put-metric-alarm --alarm-name awsrds-${RDS_IDENTIFIER}-Low-Free-Storage-Space --alarm-description "DB Storage free space is lower than 1GB" --metric-name "FreeStorageSpace" --namespace "AWS/RDS" --statistic Average --period 300 --threshold 1073741824 --comparison-operator LessThanOrEqualToThreshold --dimensions Name=DBInstanceIdentifier,Value=${RDS_IDENTIFIER} --evaluation-periods 1 --alarm-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm" --ok-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm" --insufficient-data-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm"
+aws cloudwatch put-metric-alarm --alarm-name awsrds-${RDS_IDENTIFIER}-Low-Free-Storage-Space --alarm-description "DB Storage free space is lower than 1GB" --metric-name "FreeStorageSpace" --namespace "AWS/RDS" --statistic Average --period 300 --threshold 10737418240 --comparison-operator LessThanOrEqualToThreshold --dimensions Name=DBInstanceIdentifier,Value=${RDS_IDENTIFIER} --evaluation-periods 1 --alarm-actions "${SNS_TOPIC_ARN}" --ok-actions "${SNS_TOPIC_ARN}" --insufficient-data-actions "${SNS_TOPIC_ARN}"
 
-aws cloudwatch put-metric-alarm --alarm-name awsrds-${RDS_IDENTIFIER}-Low-Freeable-Memory --alarm-description "DB Free Memory is lower than 1GB" --metric-name "FreeableMemory" --namespace "AWS/RDS" --statistic Average --period 300 --threshold 1073741824 --comparison-operator LessThanOrEqualToThreshold --dimensions Name=DBInstanceIdentifier,Value=${RDS_IDENTIFIER} --evaluation-periods 1 --alarm-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm" --ok-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm" --insufficient-data-actions "arn:aws:sns:us-west-1:261991560536:sns-adp-nonprod-alarm"
+aws cloudwatch put-metric-alarm --alarm-name awsrds-${RDS_IDENTIFIER}-Low-Freeable-Memory --alarm-description "DB Free Memory is lower than 1GB" --metric-name "FreeableMemory" --namespace "AWS/RDS" --statistic Average --period 300 --threshold 1073741824 --comparison-operator LessThanOrEqualToThreshold --dimensions Name=DBInstanceIdentifier,Value=${RDS_IDENTIFIER} --evaluation-periods 1 --alarm-actions "${SNS_TOPIC_ARN}" --ok-actions "${SNS_TOPIC_ARN}" --insufficient-data-actions "${SNS_TOPIC_ARN}"
